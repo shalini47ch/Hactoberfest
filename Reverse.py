13c013bb4a90aba966dc160dca863ab0eb2c078c
Shalini Choudhary <60210475+shalini47ch@users.noreply.github.com>
2020-10-18 12:11:13 +0530

    Update Reverse.py

--- Reverse.py
+++ Reverse.py
@@ -1,11 +1,12 @@
 def reverse(arr,n):
     start=0
-	end=n-1
-	while(start<end):
-	  temp=arr[start]
-	  arr[start]=arr[end]
-	  arr[end]=temp
-	 return arr
+    end=n-1
+    while(start<end):
+	 temp=arr[start]
+	 arr[start]=arr[end]
+	 arr[end]=temp
+     return arr
 arr=[1,4,6,8]
 n=len(arr)
 reverse(arr,n)
+#did proper indendation
